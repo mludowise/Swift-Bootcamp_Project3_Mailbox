daedce03efb171dbc853551561e309abcf96a46d
mludowise <mludowise@gmail.com>
2014-10-18 18:28:35 -0700

    Figured out animation

--- Mailbox/MessageViewController.swift
+++ Mailbox/MessageViewController.swift
@@ -10,39 +10,11 @@ import UIKit
 
 class MessageViewController: UIViewController {
     
-    private let kDragThreshold1 : CGFloat = 0.25
-    private let kDragThreshold2 : CGFloat = 0.75
-    
-    @IBOutlet weak var messageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        println("init")
+//        println(messageView.frame.origin.x)
     }
-
-    @IBAction func onMessagePan(panGestureRecognizer: UIPanGestureRecognizer) {
-//        var point = panGestureRecognizer.locationInView(view)
-        var velocity = panGestureRecognizer.velocityInView(view)
-        var translation = panGestureRecognizer.translationInView(messageView)
-        println(translation)
-        
-        var screenWidth = UIScreen.mainScreen().bounds.width
-        
-        if (translation.x < 0) { // Swipe left
-            if (translation.x > -screenWidth * kDragThreshold1) { // Gray Schedule
-                
-            } else { // Yellow Schedule
-                
-            }
-        } else { // Swipe right
-            if (translation.x < screenWidth * kDragThreshold1) { // Gray archive
-                
-            } else if (translation.x < screenWidth * kDragThreshold2) { // Green archive
-                
-            } else { // Red Delete
-                
-            }
-        }
-    }
-}
+    
+ }
